7fa2b0465652c6c81a7191a3a2da458ca3b16306
Cameron Booth <cdb@github.com>
2022-09-12 15:02:15 -0700

    .

---
+++ raycast/restart-logi-options.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Restart Logi Options
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon images/logi.png
+
+# Documentation:
+# @raycast.description Restart Logi Options
+# @raycast.author Chris
+# @raycast.authorURL @cpenned on Twitter
+
+killall LogiMgrDaemon
